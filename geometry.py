fd971fb4a2d00205872c49e86757d2b828fde623
seetharamanrs <seetharaman2632001@gmail.com>
2025-06-15 12:44:38 +0100

    Changes in Round function

--- geometry.py
+++ geometry.py
@@ -3,11 +3,11 @@ class geometry:
   def __init__(self) -> None:
     pass
   def circle(self,r):
-    return f"The area of circle is {pi*((r)**2)} and circumference is{2*pi*r}"
+    return f"The area of circle is {round(pi*((r)**2),2)} and circumference is{2*pi*r}"
   def rect(self,l,w):
     return f"The area of rectangle is {l*w} and circumference is {(2*l)+(2*w)}"
   def traingle(self,a,b,c,h):
-    return f"The area of the traingle is {0.5*b*h} and perimeter is {a+b+c}"
+    return f"The area of the traingle is {round(0.5*b*h,2)} and perimeter is {a+b+c}"
 class geometric_sequence:
     def __init__(self,a,r,n):
         self.a=a
